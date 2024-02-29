e4632d4e1fa0b0158ab3ef3e34cf56760d09859d
unknown <samiianteneh5@gmail.com>
2024-02-29 17:41:13 +0300

    admin start

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import { Route, Router, Routes } from "react-router";
 import "./App.css";
-import Detail from "./Detail/Detail";
-import HomePage from "./home/HomePage";
-import NavLink from "./NavLink/Navigation";
 import { BrowserRouter } from "react-router-dom";
-import Navigation from "./NavLink/Navigation";
-import Footer from "./Footer/Footer";
-import WhatWeDo from "./whatWeDo/WhatWeDo";
-import WhoWeAre from "./WhoWeAre/WhoWeAre";
-import Header from "./NavLink/header";
-import Testimonials from "./testimonials/Testimonials";
-import BeMember from "./beMember/BeMember";
 import { LandingPage } from "./landinPage/LandingPage";
+import LoginPage from "./login/loginPage";
+import Admin from "./Admin/admin";
 
 function App() {
   return (
@@ -21,8 +13,9 @@ function App() {
       <BrowserRouter>
         <div className="">
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/Detail" element={<Detail />} />
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/admin" element={<Admin />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ src/login/loginPage.jsx
@@ -0,0 +1,98 @@
+import axios from "axios";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", {
+        email: data.email,
+        password: data.password,
+      })
+      .then((response) => {
+        if (response.data) {
+          alert("Successfully registered");
+        }
+        console.log(response?.data, "@#$#@");
+      })
+      .catch((err) => {
+        console.error("Error", err);
+      });
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="max-w-[90%] md:max-w-md mx-auto"
+        >
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="example@example.com"
+              id="email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Invalid email address",
+                },
+              })}
+              className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-sm font-medium">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Type your password"
+              id="password"
+              {...register("password", {
+                required: "Password is required",
+                pattern: {
+                  value:
+                    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()-_=+{};:,<.>]).{8,}$/,
+                  message:
+                    "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, and be at least 8 characters long",
+                },
+              })}
+              className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+            />
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-[#43a440] text-white py-2 px-4 rounded-md hover:bg-[#296c27]"
+          >
+            Sign in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
