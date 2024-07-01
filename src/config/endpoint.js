e0febe43ec11a919ce1ac6776ae7c2744f940243
samiianteneh <samiianteneh5@gmail.com>
2024-07-01 17:08:50 +0300

    update the end point

--- src/config/endpoint.js
+++ src/config/endpoint.js
@@ -1,3 +1,4 @@
 // export const API_BASE_URL = "https://ngh1.org/back/v1";
-export const API_BASE_URL = "https://charity-project.onrender.com/v1";
-// export const API_BASE_URL = "http://172.16.32.190:3000/v1";
+// export const API_BASE_URL = "https://charity-project.onrender.com/v1";
+export const API_BASE_URL = "http://172.16.33.156:8000/api";
+export const IMG_BASE_URL = "http://172.16.33.156:8000/storage";
